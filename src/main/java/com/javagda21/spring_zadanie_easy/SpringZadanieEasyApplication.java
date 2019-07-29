a82a50906d247c38cecb2d4ed560a98166437f42
Andrzej <rubetsav@gmail.com>
2019-07-29 14:41:16 +0200

    Etap 1

---
+++ src/main/java/com/javagda21/spring_zadanie_easy/SpringZadanieEasyApplication.java
@@ -0,0 +1,13 @@
+package com.javagda21.spring_zadanie_easy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringZadanieEasyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringZadanieEasyApplication.class, args);
+    }
+
+}
